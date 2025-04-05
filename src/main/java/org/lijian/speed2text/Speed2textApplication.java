689ceb31be759bf0ef32ae7adcdb314b5fb04612
lijian <lijianlong1314@outlook.com>
2025-04-05 14:35:24 +0800

    初始化一个项目

---
+++ src/main/java/org/lijian/speed2text/Speed2textApplication.java
@@ -0,0 +1,13 @@
+package org.lijian.speed2text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Speed2textApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Speed2textApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lijian/speed2text/Speed2textApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lijian.speed2text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Speed2textApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
